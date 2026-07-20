03652bbffdbe829917b5dca45f07163e6577b1fb
Malik <malikmatoha19052003@gmail.com>
2026-07-20 18:57:49 +0800

    feat: implement multi-step screening questionnaire and respondent data collection flow

--- resources/views/pages/questionnaire.blade.php
+++ resources/views/pages/questionnaire.blade.php
@@ -166,7 +166,7 @@
             x-transition:leave-start="opacity-100 translate-y-0"
             x-transition:leave-end="opacity-0 -translate-y-2"
             class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 border border-red-200" role="alert">
-            <svg class="shrink-0 inline w-4 h-4 mr-3 mt-[2px]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <svg class="shrink-0 inline w-4 h-4 mr-3 mt-0.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
             </svg>
             <span x-text="validationError"></span>
@@ -575,7 +575,7 @@
         {{-- Loading Overlay --}}
         <template x-teleport="body">
             <div x-show="isLoading" x-cloak
-                class="fixed top-0 left-0 w-screen h-screen z-[9999] flex flex-col items-center justify-center bg-white/90 backdrop-blur-sm"
+                class="fixed top-0 left-0 w-screen h-screen z-9999 flex flex-col items-center justify-center bg-white/90 backdrop-blur-sm"
                 x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0"
                 x-transition:enter-end="opacity-100">
                 <lottie-player src="{{ asset('img/heartbeat.json') }}" background="transparent" speed="1"
